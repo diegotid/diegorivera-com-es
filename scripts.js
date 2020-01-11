8978ca51f69e97373047565d84eacaca246b97cb
Diego Rivera <diego@cuatro.studio>
2020-01-11 18:47:35 +0100

    New version draft

--- scripts.js
+++
@@ -1,7 +0,0 @@
-
-function goToSection(index) {
-  for (var i = 1; i <= 3; i++) {
-    document.getElementById("sect" + i).className = (index == i ? "" : (i == 1 ? "bnw" : "off"));
-    document.getElementById("desc" + i).className = (index == i ? "" : "off");
-  }
-}
